2c7efc41dd2f5a5a47771464b2670eafc01db7f8
mehradamiri <mehrad84001@gmail.com>
2023-04-10 10:13:03 -0700

    first commit

---
+++ main.js
@@ -0,0 +1,29 @@
+const ball = document.getElementById("ball");
+const Rekt = ball.getBoundingClientRect();
+const ballX = Rekt.left + Rekt.width / 2;
+const ballY = Rekt.top + Rekt.height / 2;
+const result = document.getElementById("result");
+document.addEventListener("mousemove", (e) => {
+    const mouseX = e.clientX;
+    const mouseY = e.clientY;
+    positionElement(mouseX,mouseY)
+
+    const angleDeg = angle(mouseX , mouseY , ballX , ballY);
+
+    // console.log(angleDeg);
+    result.innerHTML = Math.round(angleDeg)+ "°" ;
+
+})
+
+const angle = (cx,cy,ex,ey) => {
+    const dy = ey - cy;
+    const dx = ex - cx;
+    const red = Math.atan2(dy,dx);
+    const deg = red * 180 / Math.PI;
+    return deg;
+}
+
+
+const positionElement = (mouseX,mouseY)=> { 
+    result.style.transform = `translate3d(${mouseX}px, ${mouseY - 200}px, 0)`;
+  }
